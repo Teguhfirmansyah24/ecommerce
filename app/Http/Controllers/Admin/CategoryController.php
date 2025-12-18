8c3bf0826d40bcfc1ec9ec09f68a78bd5aeab9d3
Teguhfirmansyah24 <firmansyah2448@gmail.com>
2025-12-18 15:32:14 +0700

    profil dan home

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class ProfileController extends Controller
+class CategoryController extends Controller
 {
     //
 }
